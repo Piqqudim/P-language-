d9f5cdf66968d89d6e320658d50e931937f53a9d
Priesthoo <kehindeibirogba42@gmail.com>
2026-08-07 12:59:40 +0100

    added expect indent

--- p-parser/src/parser.rs
+++ p-parser/src/parser.rs
@@ -37,4 +37,53 @@ impl<'t> Parser<'t> {
     fn peek(&self) -> &TokenKind {
         &self.tokens[self.pos].kind
     }
+    fn peek_span(&self) -> Span {
+        self.tokens[self.pos].span
+    }
+    fn err(&self, msg: impl  Into<String>) -> ParseError{
+        let s = self.peek_span();
+        ParseError { file: s.file, message: msg.into(), line: s.line, col: s.col }
+    }
+
+    fn advance(&mut self)-> Token {
+        let t = self.tokens[self.pos].clone();
+        self.last_span = t.span;
+        if self.pos + 1 < self.tokens.len(){
+            self.pos += 1;
+        }
+        t
+    }
+
+    fn check(&self, kind: &TokenKind)-> bool {
+        std::mem::discriminant(self.peek()) == std::mem::discriminant(kind)
+    }
+    //Let eat here hahahahaha
+    fn eat(&mut self, kind : &TokenKind)-> bool {
+        if self.check(kind) {
+            self.advance();
+            true
+        } else {
+            false
+        }
+    }
+
+    fn expect(&mut self, kind : &TokenKind) -> Result<Token,ParseError>{
+        if self.check(&kind) {
+            Ok(self.advance())
+
+        } else {
+            Err(self.err(format!("expected {kind}, found {}", self.peek())))
+        }
+    }
+
+    fn expect_indent(&mut self)-> Result<String,ParseError>{
+        match self.peek().clone(){
+            TokenKind::Ident(s)=>{
+                self.advance();
+                Ok(s)
+            }
+            other => Err(self.err(format!("expected identifier, found {other}")))
+        }
+    }
+
 }
